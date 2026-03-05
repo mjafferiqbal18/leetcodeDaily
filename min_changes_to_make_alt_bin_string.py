0d681855a7c95194c2bbb1d04d8c675013c69da4
mjafferiqbal18 <mjafferiqbal18@gmail.com>
2026-03-05 13:21:13 -0500

    5 march 25 optimized

--- min_changes_to_make_alt_bin_string.py
+++ min_changes_to_make_alt_bin_string.py
@@ -1,12 +1,13 @@
 class Solution:
-    def minOperations(self, s: str) -> int:
-        """
+    """
         Problem: 1758. Minimum Changes To Make Alternating Binary String
         https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string/
 
         Intuition:
         - You build the two possibilities of strings (starting with 0 or starting with 1)
         - Then calculate mismatches and return the minimum
+        - You can also observe that the two counts have the following property:
+            - if c1 = x, c2 = n-x
 
         Time:
         - O(n)
@@ -14,13 +15,34 @@ class Solution:
         Space:
         - O(n)
         
-        """
+    """
+    def minOperations(self, s: str) -> int:
+        return self.optimized(s)
         n = len(s)
         s = list(s)
         s1 = ['0' if i%2==0 else '1' for i in range(n)]
         s2 = ['1' if i%2==0 else '0' for i in range(n)]
 
         return min(self.calculateOps(s,s1,n),self.calculateOps(s,s2,n))
+    
+    def optimized(self,s):
+        """
+        We'll try to do in O(1) space and O(n) time
+        We observe that count1 = x, then count2 = n - x
+        Lets try to do in O(1) space
+        """
+        n = len(s)
+        zero = True
+        count = 0
+
+        #we only check the pattern starting from 0 
+        for i in range(n):
+            if (zero and s[i] == '1') or (not zero and s[i]=='0'):
+                count += 1
+            zero = not zero
+        
+        return min(count, n - count)
+
 
     def calculateOps(self,s,sPrime,n):
         res = 0
